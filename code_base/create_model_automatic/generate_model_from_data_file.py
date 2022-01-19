231f67ae78d1f7756e11cd5b4c3a14e30e5df639
xtvarozek@stuba.sk <xtvarozek@stuba.sk>
2022-01-19 18:18:21 +0100

    feat: iplemented auto model.h file creation

--- code_base/create_model_automatic/generate_model_from_data_file.py
+++ code_base/create_model_automatic/generate_model_from_data_file.py
@@ -2,7 +2,50 @@ from tkinter.ttk import Separator
 import pandas as pd
 import numpy as np
 import json
+import matplotlib.pyplot as plt
+from sklearn.metrics import f1_score, accuracy_score, f1_score, precision_score, recall_score, classification_report, confusion_matrix, plot_confusion_matrix
+from micromlgen import port
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+
 from datetime import datetime
+def export_to_c(clf):
+    LABELS = ['1','2','3'] #our existing labels
+    classMap = {} #create an empty dict
+    for i, label in zip(range(3),LABELS): #interate over the range and the labels at the same time 
+        classMap[i]=label #fill our dict
+    c_code = port(clf,classmap=classMap) #convert our model
+
+    with open("model.h", "w") as f:
+        f.write(c_code)
+        f.close()
+
+#Let's print the size of the .h file
+import os
+model_h_size = os.path.getsize("model.h")
+def create_model(df, random_state, test_split, kernel, c):
+    print(f"{datetime.now()} --- model training start")
+
+    X = df[df.columns.difference(["room"])]
+    y = df["room"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_split, random_state=random_state)
+    clf = svm.SVC(kernel = kernel, C = c, gamma = 0.001)
+
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+
+    plt.rcParams['figure.figsize'] = 10, 5
+
+    print(f"{datetime.now()} --- model trained succesfully:")
+
+    print(f1_score(y_test, y_pred, average='micro'))
+    print(precision_score(y_test, y_pred, average="macro"))
+    print(recall_score(y_test, y_pred, average="macro"))
+
+    print(classification_report(y_test,y_pred))
+
+    return clf
 
 def df_clear(df):
     df.columns = ["room","1","n","2","n","3","n"]
@@ -33,6 +76,10 @@ def main():
     out_file = config["out_file"]
     separator = config["separator"]
 
+    random_state = config["random_state"]
+    test_split = config["test_split"]
+    kernel = config["kernel"]
+    c = config["c"]
     print(f"{datetime.now()} --- loading {in_file} as dataframe ")
     df = pd.read_csv(in_file, sep = separator, header = None)
 
@@ -45,5 +92,19 @@ def main():
     print(f"{datetime.now()} --- Saving dataframe as {out_file}")
     df.to_csv(out_file)
 
+    try:
+        model = create_model(df, random_state, test_split, kernel, c)
+    except Exception as e:
+        print(f"{datetime.now()} --- vytváranie modelu zlyhalo")  
+        print(f"{datetime.now()} --- error: {e} \n")    
+
+    try: 
+        export_to_c(model)
+    except Exception as e: 
+        print(f"{datetime.now()} --- exportovanie modelu zlyhalo")  
+        print(f"{datetime.now()} --- error: {e} \n")
+        exit()
+
+    print(f"{datetime.now()} --- model bol úspešne exportovaný")  
 if __name__ == "__main__":
     main()
---
+++ code_base/create_model_automatic/model.h
@@ -0,0 +1,99 @@
+#pragma once
+#include <cstdarg>
+namespace Eloquent {
+    namespace ML {
+        namespace Port {
+            class SVM {
+                public:
+                    /**
+                    * Predict class for features vector
+                    */
+                    int predict(float *x) {
+                        float kernels[8] = { 0 };
+                        float decisions[3] = { 0 };
+                        int votes[3] = { 0 };
+                        kernels[0] = compute_kernel(x,   -50.0  , -77.0  , -47.0 );
+                        kernels[1] = compute_kernel(x,   -56.0  , -75.0  , -22.0 );
+                        kernels[2] = compute_kernel(x,   -73.0  , -71.0  , -29.0 );
+                        kernels[3] = compute_kernel(x,   -59.0  , -67.0  , -57.0 );
+                        kernels[4] = compute_kernel(x,   -62.0  , -67.0  , -45.0 );
+                        kernels[5] = compute_kernel(x,   -65.0  , -70.0  , -52.0 );
+                        kernels[6] = compute_kernel(x,   -64.0  , -80.0  , -55.0 );
+                        kernels[7] = compute_kernel(x,   -64.0  , -60.0  , -39.0 );
+                        decisions[0] = -3.414607657558
+                        + kernels[0] * 0.016649712724
+                        + kernels[1] * -0.00586248447
+                        + kernels[3] * -0.010787228254
+                        ;
+                        decisions[1] = 7.04131386954
+                        + kernels[0] * 0.009293107183
+                        + kernels[6] * -0.006629838661
+                        + kernels[7] * -0.002663268522
+                        ;
+                        decisions[2] = 24.217125778397
+                        + kernels[2] * 0.615017252248
+                        + kernels[3]
+                        + kernels[4] * 0.288326890889
+                        + kernels[5] * -0.231038481431
+                        + kernels[6] * -0.672305661706
+                        - kernels[7]
+                        ;
+                        votes[decisions[0] > 0 ? 0 : 1] += 1;
+                        votes[decisions[1] > 0 ? 0 : 2] += 1;
+                        votes[decisions[2] > 0 ? 1 : 2] += 1;
+                        int val = votes[0];
+                        int idx = 0;
+
+                        for (int i = 1; i < 3; i++) {
+                            if (votes[i] > val) {
+                                val = votes[i];
+                                idx = i;
+                            }
+                        }
+
+                        return idx;
+                    }
+
+                    /**
+                    * Predict readable class name
+                    */
+                    const char* predictLabel(float *x) {
+                        return idxToLabel(predict(x));
+                    }
+
+                    /**
+                    * Convert class idx to readable name
+                    */
+                    const char* idxToLabel(uint8_t classIdx) {
+                        switch (classIdx) {
+                            case 0:
+                            return "1";
+                            case 1:
+                            return "2";
+                            case 2:
+                            return "3";
+                            default:
+                            return "Houston we have a problem";
+                        }
+                    }
+
+                protected:
+                    /**
+                    * Compute kernel between feature vector and support vector.
+                    * Kernel type: linear
+                    */
+                    float compute_kernel(float *x, ...) {
+                        va_list w;
+                        va_start(w, 3);
+                        float kernel = 0.0;
+
+                        for (uint16_t i = 0; i < 3; i++) {
+                            kernel += x[i] * va_arg(w, double);
+                        }
+
+                        return kernel;
+                    }
+                };
+            }
+        }
+    }
